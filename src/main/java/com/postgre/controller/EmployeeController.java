83fcf570c5f3b5eb0bd82039823a06834f3a3371
bharatbsahu <smartbharat@gmail.com>
2024-03-18 18:20:38 +0530

    Spring boot with JPA with PostGre SQL with Swagger2 Implementation

--- src/main/java/com/postgre/controller/EmployeeController.java
+++ src/main/java/com/postgre/controller/EmployeeController.java
@@ -7,6 +7,9 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,17 +17,20 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
+@Tag(name = "Employee Details Specification", description = "Employee Details")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @GetMapping("/employees")
+    @ApiOperation(value = "Return All the Employee Details")
     public java.util.List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
 
     @GetMapping("/employees/{id}")
+    @ApiOperation(value = "Return the Employee Details By ID")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
             throws ResourceNotFoundException {
         Employee employee = employeeRepository.findById(employeeId)
@@ -33,11 +39,13 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
+    @ApiOperation(value = "Add New Employee Details By ID")
     public Employee createEmployee( @RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
     @PutMapping("/employees/{id}")
+    @ApiOperation(value = "Edit Employee Details")
     public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
                                                     @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
         Employee employee = employeeRepository.findById(employeeId)
@@ -51,6 +59,7 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/employees/{id}")
+    @ApiOperation(value = "Delete the Employee Details")
     public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
             throws ResourceNotFoundException {
         Employee employee = employeeRepository.findById(employeeId)
